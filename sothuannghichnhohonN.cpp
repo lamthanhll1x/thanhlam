c9589e02397b775948980cd01c5d8c360ca85254
lamthanhll1x <53205979+lamthanhll1x@users.noreply.github.com>
2019-08-02 06:54:29 +0700

    Update sothuannghichnhohonN.cpp

--- sothuannghichnhohonN.cpp
+++ sothuannghichnhohonN.cpp
@@ -1,34 +1,20 @@
-#include "stdio.h"
-
-int SoThuanNghich(int N);
-int main()
-{
-	int N, i;
-	do
-	{
-		printf("\nNhap vao so N= ");
-		scanf("%d", &N);
-		
-	}
-	while(N <= 0);
-	printf("\nCac so thuan nghich nho hon %d :",N);
-	for(i = 1; i <= N; i++)
-	{
-		if (SoThuanNghich(i))
-			printf("%d ", i);	
-	}
-	   return 0;
-	   
+#include<stdio.h>
+	 int main(){
+	 	int n,m,tg;
+	 	printf("nhap so n:",n);
+	 	scanf("%d",&n);
+	 	
+	 	for(int i=1; i<=n; i++){
+	 		m=i;
+	 		tg=0;
+	 		while(m!=0){
+	 		tg=tg*10 + m%10;
+	 		m/=10;
 }
- //Kiem tra so thuan nghich
-int SoThuanNghich(int N)
-{
-	int tg =N;
-	int temp = 0;
-	while(N>0)
-	{
-		temp = temp*10 + N%10;
-		N/=10;
-	}
-	return( temp == tg);
+		 if(tg==i){
+		 	printf("%d\n",tg);
+		 }
 }
+	 	return 0;
+	 	
+	 }
